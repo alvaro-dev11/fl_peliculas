ef2a111f6379cc0325d4f796c6e9cf2f4c86d6d4
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-11-21 20:24:50 -0500

    Terminar diseño de los detalles de la película.

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,14 +1,131 @@
+import 'package:fl_peliculas/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-   
   const DetailsScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('DetailsScreen'),
+    // TODO: Cambiar luego por una instancia de movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no movie';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const _CustomerAppBar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            const _PosterAndTitle(),
+            const _Overview(),
+            const _Overview(),
+            const _Overview(),
+            const _Overview(),
+            const CastingCart(),
+          ]))
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomerAppBar extends StatelessWidget {
+  const _CustomerAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          padding: const EdgeInsets.only(bottom: 10),
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16, color: Colors.white),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle();
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headlineSmall,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle',
+                style: textTheme.titleSmall,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  const SizedBox(width: 5),
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.labelSmall,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: const Text(
+        'Commodo commodo amet laborum consequat dolor culpa velit dolore. Qui amet ut amet id amet ad id sit ullamco labore cupidatat magna aliquip incididunt. Fugiat eiusmod incididunt cillum ea ut sunt labore tempor incididunt enim eu sit consectetur. Do sunt deserunt deserunt consectetur nulla enim fugiat do consectetur ipsum officia culpa.',
+        textAlign: TextAlign.justify,
+        style: TextStyle(fontSize: 16),
       ),
     );
   }
