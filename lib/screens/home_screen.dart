b837189a39cc29a536d0aaa37f440379901bd981
Alvaro Antony Villanueva Barreto <barretoalvaro369@gmail.com>
2024-11-22 12:54:11 -0500

    Mostrar las imágenes de las peliculas.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,15 @@
+import 'package:fl_peliculas/providers/movies_provider.dart';
 import 'package:fl_peliculas/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Películas en cines'),
@@ -15,14 +19,14 @@ class HomeScreen extends StatelessWidget {
                 onPressed: () {}, icon: const Icon(Icons.search_outlined))
           ],
         ),
-        body: const SingleChildScrollView(
+        body: SingleChildScrollView(
           child: Column(
             children: [
               // Tarjetas principales
-              CardSwiper(),
+              CardSwiper(movies: moviesProvider.onDisplayMovie),
 
               // Slider de películas
-              MovieSlider()
+              const MovieSlider()
             ],
           ),
         ));
